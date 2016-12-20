ab4fbf78bc62eeb2f1948738ebed20d13ffd43d2
BrutalMaster9854 <brutalmaster9854@gmail.com>
2016-12-20 14:51:18 -0600

    Added PlaceHolderAPI && updated to 1.3

--- src/tk/brutalmaster9854/core/packet/actionbar/PacketActionbar.java
+++ src/tk/brutalmaster9854/core/packet/actionbar/PacketActionbar.java
@@ -10,30 +10,25 @@ import java.lang.reflect.Method;
 public class PacketActionbar extends BPacket {
 
     public void sendActionBar(Player player, String message) {
-        message = BrutalCore.get().toColor(message);
 
-        String nmsver = getNMSVersion();
         try {
-            Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-            Object p = c1.cast(player);
 
-            Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
-            Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+            Class<?> craftPlayer = getNMSClass("CraftPlayer");
+            Class<?> packetPlayOutChat = getNMSClass("PacketPlayOutChat");
+            Class<?> packet = getNMSClass("Packet");
 
-            Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-            Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-            Object o = c2.getConstructor(new Class[] { String.class }).newInstance(new Object[] { message });
-            Object ppoc = c4.getConstructor(new Class[] { c3, Byte.TYPE }).newInstance(new Object[] { o, Byte.valueOf("2") });
+            Object p = craftPlayer.cast(player);
 
-            Method m1 = c1.getDeclaredMethod("getHandle", new Class[0]);
-            Object h = m1.invoke(p, new Object[0]);
-            Field f1 = h.getClass().getDeclaredField("playerConnection");
-            Object pc = f1.get(h);
-            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", new Class[] { c5 });
-            m5.invoke(pc, new Object[] { ppoc });
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
+            Class<?> chatComponentText = getNMSClass("ChatComponentText");
+            Class<?> iChatBaseComponent = getNMSClass("IChatBaseComponent");
+
+            Object o = chatComponentText.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+            Object ppoc = packetPlayOutChat.getConstructor(new Class<?>[]{iChatBaseComponent, byte.class}).newInstance(o, (byte)2);
+
+            sendPacket(player, ppoc);
+
+        } catch (Exception ex) {
+
         }
     }
 }
