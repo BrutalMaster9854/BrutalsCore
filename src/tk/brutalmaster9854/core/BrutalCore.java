df229d4c1ca0f787d67eceefdbdbb8c6f99c935b
BrutalMaster9854 <brutalmaster9854@gmail.com>
2016-12-20 14:54:26 -0600

    Added PlaceHolderAPI && updated to 1.3

--- src/tk/brutalmaster9854/core/BrutalCore.java
+++ src/tk/brutalmaster9854/core/BrutalCore.java
@@ -38,6 +38,8 @@ public class BrutalCore extends JavaPlugin {
         packetTabList = new PacketTabList();
         placeHolderAPI = new PlaceHolderAPI();
 
+        System.out.println();
+
         registerListeners(this, new Example());
 
         getServer().getScheduler().runTaskAsynchronously(this, new Runnable() {
